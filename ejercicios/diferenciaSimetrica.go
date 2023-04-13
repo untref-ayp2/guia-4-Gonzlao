01b848eebe72b4672bc267dd7fbc16e20146e792
Gonzalo Giuffre <gonzalogiuffre@MacBook-Pro-de-Gonzalo.local>
2023-04-13 14:17:50 -0300

    Ejercicios resueltos

--- ejercicios/diferenciaSimetrica.go
+++ ejercicios/diferenciaSimetrica.go
@@ -5,5 +5,8 @@ import (
 )
 
 func DiferenciaSimetrica[T comparable](s1, s2 *set.Set[T]) *set.Set[T] {
-	panic("Not implemented")
+	d1 := set.Difference(s1, s2)
+	d2 := set.Difference(s2, s1)
+
+	return set.Union(d1, d2)
 }
